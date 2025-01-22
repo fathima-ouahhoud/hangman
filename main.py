74c3acb15cc7aeb45f2f5f08b848e5e6f1b46e9d
Linda Choucha <mehdi.choucha@laplateforme.io>
2025-01-22 14:33:29 +0100

    :sparkles: made a list of words

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import pygame
 pygame.init()
 pygame.display.set_caption("Jeu du pendu")
-file = open("mots.txt", "r")
-read = file.readline()
+
+screen = pygame.display.set_mode((800, 500))
+black = [0, 0, 0]
+white = [255, 255, 255]
+image = pygame.image.load('C:/Users/linda/OneDrive/Bureau/pendu/images/pendu_1.png').convert()
+arial_font = pygame.font.SysFont("Arial", 22)
+order = arial_font.render("Choissisez les bonnes lettres pour compléter le mot", False, black)
 
 running = True
-screen = pygame.display.set_mode((800, 500))
-white = [255, 255, 255]
-black = [0, 0, 0]
-image = pygame.image.load('C:/Users/linda/OneDrive/Bureau/pendu/images/pendu_1.png').convert()
-arial_font = pygame.font.SysFont("Arial", 25)
-order = arial_font.render("Choissisez les bonnes lettres pour compléter le mot", True, black)
-
-#plesk > httpdocs > index 
-#repository pour actualiser à partir de git
 
 while running:
 
@@ -23,7 +19,6 @@ while running:
             running = False
 
     screen.blit(order, (30, 20))
-    screen.blit(read, (20, 30))
 
     screen.blit(image, (460, 79))
 

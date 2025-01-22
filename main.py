8eaf9eaf8bfb4d473aa595dd6620bf76feea8cf2
Linda Choucha <mehdi.choucha@laplateforme.io>
2025-01-22 08:22:58 +0100

    :sparkles: An image can be fully display

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((1200, 750))
+screen = pygame.display.set_mode((800, 500))
+pygame.display.set_caption("Jeu du pendu")
 running = True
+white = [255, 255, 255]
 
-image = pygame.image.load('pendu_1.png').convert()
+
+image = pygame.image.load('C:/Users/linda/OneDrive/Bureau/pendu/images/pendu_1.png').convert()
 
 while running:
     for event in pygame.event.get():
+        screen.fill(white)
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(image, (0, 0))
+    screen.blit(image, (450, 79))
     pygame.display.flip()
 
 pygame.quit()
